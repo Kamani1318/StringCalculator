97269d7ca83f28565ee0f14f6f0f21102c726715
Aryan Kamani <aryankamani@aryans-air.lan>
2023-01-17 15:53:45 +0530

    Passed test with two numbers

--- src/iter_1.py
+++ src/iter_1.py
@@ -1,8 +1,15 @@
+import re
+from functools import reduce
+def sum(x,y):
+    return int(x) + int(y)
 def string_calc(str):
     if str == "":
         return 0
     else:
-        return int(str)
+        numbers = re.split(",",str)
+        res = [eval(i) for i in numbers]
+        print(res)
+        return reduce(lambda x,y: x + y, res)
 
 
             
